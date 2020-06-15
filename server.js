045451df1b2e70c0703384f6fc1603b6e77b6777
Abraham Vega <abrahamvega987@gmail.com>
2020-06-15 00:10:13 -0500

    init directory on server changed

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/spa'));
+app.use(express.static(__dirname + '/dist/spotiapp'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
+res.sendFile(path.join(__dirname+'/dist/spotiapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
